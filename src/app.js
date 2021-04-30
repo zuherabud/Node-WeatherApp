801cccd8fd1eb091f367b3b738782bc24bbc9d40
zuherabud <zuherabud@gmail.com>
2021-04-30 15:54:29 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const hbs =  require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -17,8 +19,8 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-app.listen(3000, ()=> {
-    console.log('Server is listening on port 3000')
+app.listen(port, ()=> {
+    console.log('Server is listening on port' + port)
 })
 
 app.use(express.static(publicDirectoryPath))
